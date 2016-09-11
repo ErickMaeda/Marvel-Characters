5219a7bc90f83dcb699bd9b0545217cf8e6be15e
Erick <erick.maeda26@gmail.com>
2016-09-11 20:53:44 -0300

    minor bugs fixed

--- app/src/main/java/marvel/erickmaeda/com/marvelcharacters/system/utils/Constants.java
+++ app/src/main/java/marvel/erickmaeda/com/marvelcharacters/system/utils/Constants.java
@@ -2,28 +2,27 @@ package marvel.erickmaeda.com.marvelcharacters.system.utils;
 
 public class Constants {
 
+    /**
+     * Put here your private and public keys generated on http://developer.marvel.com/
+     */
     public static final class Keys {
-        /**
-         * Put here your private and public keys generated on http://developer.marvel.com/
-         */
         public static final String PRIVATE_KEY = "";
         public static final String PUBLIC_KEY = "";
     }
 
+    /**
+     * Main URL from API http://developer.marvel.com/
+     * if the version changes please change here the URL
+     */
     public class Settings {
-        /**
-         * Main URL from API http://developer.marvel.com/
-         * if the version changes please change here the URL
-         */
         public static final String MAIN_URL = "https://gateway.marvel.com:443/v1/public/";
         public static final String CHARACTERS_URL = MAIN_URL + "characters?apikey=" + Keys.PUBLIC_KEY;
     }
 
+    /**
+     * Image sizes from API http://developer.marvel.com/
+     */
     public static final class ImagesSize {
-
-        /**
-         * Image sizes from API http://developer.marvel.com/
-         */
         public static final String PORTRAIT_SMALL = "portrait_small";
         public static final String PORTRAIT_MEDIUM = "portrait_medium";
         public static final String PORTRAIT_XLARGE = "portrait_xlarge";
@@ -32,10 +31,11 @@ public class Constants {
         public static final String PORTRAIT_INCREDIBLE = "portrait_incredible";
     }
 
+    /**
+     * This class is responsible to record the keys about SavedInstanceState
+     */
     public static final class SavedInstanceState {
-        /**
-         * This class is responsible to record the keys about SavedInstanceState
-         */
+
         public static final class Characters {
             public static final String LAST_ITEM_SEARCHED = "last_item_searched";
         }
